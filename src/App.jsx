c5dbd01f815477304676a196923cf082713013e0
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-04-22 14:26:52 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -41,16 +41,6 @@ function App() {
                 <a href="#">Support</a>
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
               </div>
-              <div class="attribution">
-                Challenge by{" "}
-                <a
-                  href="https://www.frontendmentor.io?ref=challenge"
-                  target="_blank"
-                >
-                  Frontend Mentor
-                </a>
-                . Coded by <a href="#">Your Name Here</a>.
-              </div>
             </div>
             {/* @TO DO HAMBURGER BUTTON */}
 
